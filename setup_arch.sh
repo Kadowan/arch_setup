1737235fa328ec14fbafa998e55109cb9868d575
Kadowan <52757316+Kadowan@users.noreply.github.com>
2023-07-12 21:50:01 +0200

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -8,9 +8,20 @@ gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward ['<S
 gsettings set org.gnome.desktop.wm.keybindings switch-windows ['<Alt>Tab']
 gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward ['<Shift><Alt>Tab']
 
-# set custom gnome shortcuts
+# set starting gnome shortcuts
 gsettings set org.gnome.settings-daemon.plugins.media-keys calculator ['<Super>r']
 gsettings set org.gnome.settings-daemon.plugins.media-keys home ['<Super>f']
 gsettings set org.gnome.settings-daemon.plugins.media-keys www ['<Super>i']
 gsettings set org.gnome.settings-daemon.plugins.media-keys email ['<Super>n']
 
+# set custom gnome keybindings
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'open Gnome Terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>t'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'systemctl poweroff'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'systemctl poweroff'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Control><Super>s'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name 'Hibernate'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command 'systemctl hibernate'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding '<Control><Super>h'
