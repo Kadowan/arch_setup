880585d17f49caf51bbdf9a59c21cc0c200cb43c
Kadowan <52757316+Kadowan@users.noreply.github.com>
2023-07-12 21:58:52 +0200

    fixed quotation marks for gsettings

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # set gnome shortcuts
-gsettings set org.gnome.desktop.wm.keybindings close ['<Super>q']
-gsettings set org.gnome.desktop.wm.keybindings maximize ['<Super>Up']
-gsettings set org.gnome.desktop.wm.keybindings switch-applications ['<Super>Tab']
-gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward ['<Shift><Super>Tab']
-gsettings set org.gnome.desktop.wm.keybindings switch-windows ['<Alt>Tab']
-gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward ['<Shift><Alt>Tab']
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
 
 # set starting gnome shortcuts
-gsettings set org.gnome.settings-daemon.plugins.media-keys calculator ['<Super>r']
-gsettings set org.gnome.settings-daemon.plugins.media-keys home ['<Super>f']
-gsettings set org.gnome.settings-daemon.plugins.media-keys www ['<Super>i']
-gsettings set org.gnome.settings-daemon.plugins.media-keys email ['<Super>n']
+gsettings set org.gnome.settings-daemon.plugins.media-keys calculator "['<Super>r']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>f']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>i']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys email "['<Super>n']"
 
 # set custom gnome keybindings
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/']"
