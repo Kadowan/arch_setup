cb625fb677d322348e3f72d80cc5e5f87ab3fc35
Kadowan <52757316+Kadowan@users.noreply.github.com>
2023-07-12 21:27:43 +0200

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+# Setting gnome shortcuts
+gsettings set org.gnome.desktop.wm.keybindings close ['<Super>q']
+gsettings set org.gnome.desktop.wm.keybindings maximize ['<Super>Up']
+gsettings set org.gnome.desktop.wm.keybindings switch-applications ['<Super>Tab']
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward ['<Shift><Super>Tab']
+gsettings set org.gnome.desktop.wm.keybindings switch-windows ['<Alt>Tab']
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward ['<Shift><Alt>Tab']
